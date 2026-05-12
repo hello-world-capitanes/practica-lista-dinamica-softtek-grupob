e3c1e042164d0fdbf83103b1b60ab21b7a5bc7de
SOFTTEKPROJECTS\juand.castillo <juand.castillo@softtek.com>
2026-05-12 16:43:33 +0200

    ARREGLADO EL APP.JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-// Importamos el hook y los componentes según tu estructura de carpetas
+import React from 'react';
 import { useItems } from './Hooks/useItems'; 
 import FormContainer from './Components/Form/FormContainer';
 import List from './Components/List/List';
@@ -7,17 +6,21 @@ import Modal from './Components/Modal/Modal';
 import './App.css';
 
 function App() {
-  // Lógica del Hook (Integrante 3/Arquitecto)
-  const { items, addItem, removeItem, moveItem } = useItems();
-  
-  // Estado para controlar el Modal
-  const [selectedItem, setSelectedItem] = useState(null);
+
+  const {
+    items,
+    selectedItem,
+    addItem,
+    deleteItem,
+    moveItem,
+    selectItem,
+    closeModel
+  } = useItems();
 
   return (
     <div className="app-container">
       <h1>Gestión de Elementos Dinámicos</h1>
 
-     
       <section>
         <FormContainer onAdd={addItem} />
       </section>
@@ -27,25 +30,17 @@ function App() {
       <section>
         <List 
           items={items} 
-          onRemove={removeItem} 
+          onRemove={deleteItem} 
           onMove={moveItem} 
-          onSelect={(item) => setSelectedItem(item)} 
+          onSelect={selectItem} 
         />
       </section>
 
       
       <Modal 
-        isOpen={!!selectedItem} 
-        onClose={() => setSelectedItem(null)}
-        title="Detalles del Registro"
+        item={selectedItem} 
+        onClose={closeModel}
       >
-        {selectedItem && (
-          <div className="detail-view">
-            <p><strong>Nombre:</strong> {selectedItem.nombre}</p>
-            <p><strong>Tipo/Categoría:</strong> {selectedItem.tipo}</p>
-            <p><strong>Estado:</strong> {selectedItem.esUrgente ? "Urgente" : "Normal"}</p>
-          </div>
-        )}
       </Modal>
     </div>
   );
