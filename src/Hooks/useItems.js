e3d34788abaf128f228e0d349d321fe8083a4399
SOFTTEKPROJECTS\juand.castillo <juand.castillo@softtek.com>
2026-05-12 16:34:00 +0200

    LISTA LA CLASE useItems con funciones para agregar remover etc..

---
+++ src/Hooks/useItems.js
@@ -0,0 +1,54 @@
+import {useState} from 'react';
+
+function useItems() {
+    const [items, setItems] = useState([]);
+    const [selectedItem, setSelectedItem] = useState(null);
+
+    function addItem(newItem) {
+        const itemConId={ ...newItem, id: Date.now() };
+        setItems(prevItems => [...prevItems, itemConId]);
+    }
+
+    function deleteItem(id) {
+        setItems(prevItems => prevItems.filter(item => item.id !== id));
+    }
+
+    function moveItem(id, direction) {
+        setItems(prevItems => {
+            const index = prevItems.findIndex(item => item.id === id);
+            if (index === -1) return prevItems;
+
+            const newItems = [...prevItems];
+            const item = newItems.splice(index, 1)[0];
+
+            if (direction === 'up' && index > 0) {
+                newItems.splice(index - 1, 0, item);
+            } else if (direction === 'down' && index < prevItems.length - 1) {
+                newItems.splice(index + 1, 0, item);
+            }
+
+            return newItems;
+        });
+    }
+
+    function selectItem(id) {
+        const item = items.find(item => item.id === id);
+        setSelectedItem(item);
+    }
+
+    function closeModel(){
+        setSelectedItem(null);
+    }
+
+    return {
+        items,
+        selectedItem,
+        addItem,
+        deleteItem,
+        moveItem,
+        selectItem,
+        closeModel
+    };
+
+
+};
